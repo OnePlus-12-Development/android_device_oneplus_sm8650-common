f08468d351c509a931440f0f7f5ade53103eeb91
Nolen Johnson <johnsonnolen@gmail.com>
2021-05-09 16:04:34 -0400

    sm8550-common: Build a VBMeta with a disabled hashtree.
     * AVB's `--flags 3` argument builds a disabled VBMeta with
       disabled hashtree, so no need to set both flags individually.
    
    Change-Id: I7fce153b9bbd35be99d732af39539d81081b3ae5

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -210,8 +210,7 @@ SOONG_CONFIG_OPLUS_LINEAGE_TOUCH_HAL_INCLUDE_DIR := $(COMMON_PATH)/touch/include
 
 # Verified Boot
 BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := true
 
 BOARD_AVB_BOOT_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
