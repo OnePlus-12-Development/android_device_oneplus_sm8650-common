9353ef4f092abac50d563a49453735592fea787a
chandu078 <chandudyavanapelli03@gmail.com>
2023-05-14 13:37:00 +0000

    sm8550-common: Set board API and shipping API level to 33
     * https://git.codelinaro.org/clo/la/platform/vendor/qcom/taro/-/commit/55e9026aa2708f89013e0fcd6d766173cd12e710.
    
    Change-Id: Ie98c42d3e9c5d31341b326fe791ab8f6ef52a8e4
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- common.mk
+++ common.mk
@@ -384,7 +384,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
 
 # Shipping API
-PRODUCT_SHIPPING_API_LEVEL := 33
+BOARD_API_LEVEL := 33
+BOARD_SHIPPING_API_LEVEL := $(BOARD_API_LEVEL)
+PRODUCT_SHIPPING_API_LEVEL := $(BOARD_API_LEVEL)
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
