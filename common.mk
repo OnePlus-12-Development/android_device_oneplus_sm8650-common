5e59d3f3692e00b87fca3841820e4aca09619cca
Bruno Martins <bgcngm@gmail.com>
2023-07-22 22:56:21 +0100

    sm8550-common: Use EuiccGoogle package from stock images
    After all there's no need to steal it from elsewhere because
    it exists in the my_bigball partition, so copy it along with
    the priv-app permissions grant file.
    
    Change-Id: I0a5c3ef585174185ac969a8f6ca9bf0d0e6f2d85

--- common.mk
+++ common.mk
@@ -423,9 +423,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.telephony.mbms.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.mbms.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-euiccgoogle.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-euiccgoogle.xml
-
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti-v2
