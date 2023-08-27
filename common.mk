e45f75b2dd72e53ea4429d2cb5f41221c716ccdc
Aaron Kling <webgeek1234@gmail.com>
2023-08-27 12:54:32 -0500

    sm8550-common: Move most agm and pal targets to source built
    Change-Id: I6ca0c5396a3b583b37e029f5c98ff13127db65a6

--- common.mk
+++ common.mk
@@ -52,11 +52,17 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
     android.hardware.soundtrigger@2.3-impl \
+    agmcap \
+    agmcompressplay \
+    agmplay \
     audio.bluetooth.default \
     audio.primary.kalama \
     audio.r_submix.default \
     audio.usb.default \
     audioadsprpcd \
+    libagm_compress_plugin \
+    libagm_mixer_plugin \
+    libagm_pcm_plugin \
     libbatterylistener \
     libqcompostprocbundle \
     libqcomvisualizer \
@@ -293,7 +299,8 @@ PRODUCT_PACKAGES += \
     libavservices_minijail \
     libavservices_minijail.vendor \
     libcodec2_hidl@1.0.vendor \
-    libcodec2_vndk.vendor
+    libcodec2_vndk.vendor \
+    libpalclient
 
 # Net
 PRODUCT_PACKAGES += \
