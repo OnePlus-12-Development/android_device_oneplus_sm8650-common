8adcea206ecdfad7ae5913114bbb07b21a29e4c4
LuK1337 <priv.luk@gmail.com>
2023-12-29 09:59:31 +0100

    sm8550-common: Patch configs to drop NFC logging levels
    Co-authored-by: Georg Veichtlbauer <georg@vware.at>
    Change-Id: I42bcd622dea35dd69f4fc6bb72a4767ca69af5c3

--- extract-files.sh
+++ extract-files.sh
@@ -76,6 +76,13 @@ function blob_fixup() {
         vendor/bin/hw/android.hardware.security.keymint-service-qti)
             grep -q "android.hardware.security.rkp-V3-ndk.so" "${2}" || ${PATCHELF} --add-needed "android.hardware.security.rkp-V3-ndk.so" "${2}"
             ;;
+        vendor/etc/libnfc-nci.conf)
+            sed -i "s/NFC_DEBUG_ENABLED=1/NFC_DEBUG_ENABLED=0/" "${2}"
+            ;;
+        vendor/etc/libnfc-nxp.conf)
+            sed -i "/NXPLOG_\w\+_LOGLEVEL/ s/0x03/0x02/" "${2}"
+            sed -i "s/NFC_DEBUG_ENABLED=1/NFC_DEBUG_ENABLED=0/" "${2}"
+            ;;
     esac
 }
 
