99e23539f581f6b3ecf27c6d00a4ce9cdb45a25c
Bruno Martins <bgcngm@gmail.com>
2024-01-04 14:16:46 +0000

    sm8550-common: Update from OOS 11.C.48
    Keep using previous libextensionlayer prebuilt, since the new one
    leads camera preview to crash.
    
    Change-Id: I3298322be251db75012ec4dc1e594eaa66ac4222

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021-2023 The LineageOS Project
+# Copyright (C) 2021-2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -218,7 +218,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security
-BOOT_SECURITY_PATCH := 2023-11-05
+BOOT_SECURITY_PATCH := 2023-12-05
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # SEPolicy
