4ad006d9c5c4064634cfa8b9f4f19fbd30a1ff08
inferno0230 <mail@inferno0230.in>
2025-10-23 12:11:06 +0530

    sm8650-common: PixelOS Bringup
    Change-Id: Ia01129a99152478395a38ca5aa3a5d54768db821
    Signed-off-by: inferno0230 <mail@inferno0230.in>

--- common.mk
+++ common.mk
@@ -296,7 +296,7 @@ $(call inherit-product, hardware/oplus/overlay/generic/generic.mk)
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aosp
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
